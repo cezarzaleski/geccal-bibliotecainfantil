6575bb5b955e8eac2a6cfa26c54050f1db9f1cea
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2022-12-28 21:11:17 -0300

    fix: change type exemplary field to INT

--- src/main/resources/db/migration/V1__create_books.sql
+++ src/main/resources/db/migration/V1__create_books.sql
@@ -3,7 +3,7 @@ create table if not exists books
     id VARCHAR(36) NOT NULL PRIMARY KEY,
     name TEXT NOT NULL,
     status VARCHAR(10) NOT NULL,
-    exemplary VARCHAR(10) NOT NULL,
+    exemplary INT NOT NULL,
     edition VARCHAR(11) NOT NULL,
     authors JSON NOT NULL,
     publisher VARCHAR(255) NOT NULL,
