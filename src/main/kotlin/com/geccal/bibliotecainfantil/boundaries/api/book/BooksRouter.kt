9ddf2c76fc66d98479cff5e02e6df828546b4d1d
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2023-01-01 19:54:43 -0300

    refactor: create RequestExtensions to create SearchQuery of ApplicationRequest

--- src/main/kotlin/com/geccal/bibliotecainfantil/boundaries/api/book/BooksRouter.kt
+++ src/main/kotlin/com/geccal/bibliotecainfantil/boundaries/api/book/BooksRouter.kt
@@ -1,10 +1,9 @@
 package com.geccal.bibliotecainfantil.boundaries.api.book
 
 import com.geccal.bibliotecainfantil.boundaries.api.book.models.CreateBookRequest
+import com.geccal.bibliotecainfantil.boundaries.api.book.models.extension.toSearchQuery
 import com.geccal.bibliotecainfantil.core.application.book.create.CreateBookUseCase
 import com.geccal.bibliotecainfantil.core.application.book.retrieve.list.ListBookUseCase
-import com.geccal.bibliotecainfantil.core.domain.pagination.Pagination
-import com.geccal.bibliotecainfantil.core.domain.pagination.SearchQuery
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
@@ -29,16 +28,9 @@ fun Routing.booksRouter(
             )
         }
         get {
-            val search = call.request.queryParameters["search"].orEmpty()
-            val page = call.request.queryParameters["page"]?.toIntOrNull() ?: Pagination.DEFAULT_PAGE
-            val perPage = call.request.queryParameters["perPage"]?.toIntOrNull() ?: Pagination.DEFAULT_PER_PAGE
-            val sort = call.request.queryParameters["sort"].orEmpty()
-            val direction = call.request.queryParameters["direction"] ?: "name"
-            val searchQuery = SearchQuery(page, perPage, search, sort, direction)
-
             call.respond(
                 HttpStatusCode.OK,
-                listBookUseCase.execute(searchQuery)
+                listBookUseCase.execute(call.request.toSearchQuery())
             )
         }
     }
---
+++ src/main/kotlin/com/geccal/bibliotecainfantil/boundaries/api/book/models/extension/RequestExtensions.kt
@@ -0,0 +1,14 @@
+package com.geccal.bibliotecainfantil.boundaries.api.book.models.extension
+
+import com.geccal.bibliotecainfantil.core.domain.pagination.Pagination
+import com.geccal.bibliotecainfantil.core.domain.pagination.SearchQuery
+import io.ktor.server.request.ApplicationRequest
+
+fun ApplicationRequest.toSearchQuery(): SearchQuery {
+    val search = call.request.queryParameters["search"].orEmpty()
+    val page = call.request.queryParameters["page"]?.toIntOrNull() ?: Pagination.DEFAULT_PAGE
+    val perPage = call.request.queryParameters["perPage"]?.toIntOrNull() ?: Pagination.DEFAULT_PER_PAGE
+    val sort = call.request.queryParameters["sort"].orEmpty()
+    val direction = call.request.queryParameters["direction"] ?: "name"
+    return SearchQuery(page, perPage, search, sort, direction)
+}
