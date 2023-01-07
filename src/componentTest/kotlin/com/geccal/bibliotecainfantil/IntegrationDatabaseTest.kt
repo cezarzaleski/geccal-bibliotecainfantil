d0d3a3737e3405f1774d3880e95a4a95e87e1516
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2023-01-07 17:03:43 -0300

    chore: configure componentTest with database

--- src/componentTest/kotlin/com/geccal/bibliotecainfantil/IntegrationDatabaseTest.kt
+++ src/componentTest/kotlin/com/geccal/bibliotecainfantil/IntegrationDatabaseTest.kt
@@ -7,6 +7,20 @@ import com.geccal.bibliotecainfantil.infra.database.VertxConnectionAdapter
 @Suppress("UtilityClassWithPublicConstructor")
 open class IntegrationDatabaseTest {
 
+    init {
+        println("port")
+        println(EnvironmentConfiguration.getInt("database.config.mysql.port"))
+        println("host")
+        println(EnvironmentConfiguration.getString("database.config.mysql.host"))
+        println("username")
+        println(EnvironmentConfiguration.getString("database.config.mysql.username"))
+        println("password")
+        println(EnvironmentConfiguration.getString("database.config.mysql.password"))
+        println("database")
+        println(EnvironmentConfiguration.getString("database.config.mysql.database"))
+
+    }
+
     val port = EnvironmentConfiguration.getInt("database.config.mysql.port")
     val host = EnvironmentConfiguration.getString("database.config.mysql.host")
     val user = EnvironmentConfiguration.getString("database.config.mysql.username")
