ac60c7eac4afbb690f89fb7f7727b6cc54f63abf
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2023-01-25 22:40:25 -0300

    feat(SearchQuery): set default value page and perPage parameters

--- src/main/kotlin/com/geccal/bibliotecainfantil/core/domain/pagination/SearchQuery.kt
+++ src/main/kotlin/com/geccal/bibliotecainfantil/core/domain/pagination/SearchQuery.kt
@@ -1,8 +1,8 @@
 package com.geccal.bibliotecainfantil.core.domain.pagination
 
 data class SearchQuery(
-    val page: Int,
-    val perPage: Int,
+    val page: Int = Pagination.DEFAULT_PAGE,
+    val perPage: Int = Pagination.DEFAULT_PER_PAGE,
     val terms: String,
     val sort: String,
     val direction: String = Pagination.DEFAULT_DIRECTION
